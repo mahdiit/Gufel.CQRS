b1b2acb7160d758de670fd20b7dcc48494f97f2e
Mehdi Yousefi <yousefi.mehdi@khanoumi.com>
2025-05-05 09:58:34 +0330

    fix dispacher sample

--- Gufel.Sample/Program.cs
+++ Gufel.Sample/Program.cs
@@ -21,9 +21,9 @@ namespace Gufel.Sample
             services.RegisterDispatcher(typeof(Program).Assembly);
 
             await using var app = services.BuildServiceProvider();
-            //var publisher = app.GetRequiredService<IMessagePublisher>();
-            //var order = new OrderModel() { Id = 1, OrderCount = 22, ProductId = 13, UserId = 1300 };
-            //publisher.Publish("reg-order", order);
+            var publisher = app.GetRequiredService<IMessagePublisher>();
+            var order = new OrderModel() { Id = 1, OrderCount = 22, ProductId = 13, UserId = 1300 };
+            publisher.Publish("reg-order", order);
 
             var dispatcher = app.GetRequiredService<IDispatcher>();
             var command = new SampleRequest() { Id = 1200 };
@@ -31,7 +31,7 @@ namespace Gufel.Sample
             Console.WriteLine("Dispatch result: " + result.Result);
 
             var cmd2 = new SampleRequestNoResponse() { Id = 1300 };
-            await dispatcher.Dispatch<SampleRequestNoResponse>(cmd2, CancellationToken.None);
+            await dispatcher.Dispatch(cmd2, CancellationToken.None);
             Console.WriteLine("Dispatch result: no result ");
 
             Console.WriteLine("End");
