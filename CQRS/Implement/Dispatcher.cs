ca57cadb4181a14c688483bdd302b6bf00feed64
Mehdi Yousefi <yousefi.mehdi@khanoumi.com>
2025-05-10 15:40:17 +0330

    remove used code

--- CQRS/Implement/Dispatcher.cs
+++ CQRS/Implement/Dispatcher.cs
@@ -1,39 +1,20 @@
 ﻿using Gufel.Dispatcher.Base.Dispatcher;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Gufel.Dispatcher.Implement
 {
-    public class Dispatcher : IDispatcher, IDisposable
+    public sealed class Dispatcher(IServiceProvider serviceProvider) : IDispatcher
     {
-        private readonly IServiceScope? _serviceScope;
-        private readonly IServiceProvider _serviceProvider;
-
-        public Dispatcher(
-            IServiceScopeFactory serviceScope, 
-            IHttpContextAccessor httpContextAccessor)
-        {
-            if (httpContextAccessor.HttpContext != null)
-            {
-                _serviceProvider = httpContextAccessor.HttpContext.RequestServices;
-            }
-            else
-            {
-                _serviceScope = serviceScope.CreateScope();
-                _serviceProvider = _serviceScope.ServiceProvider;
-            }
-        }
-
         public async Task Dispatch<TRequest>(TRequest request, CancellationToken cancellation)
             where TRequest: IRequest
         {
-            var pipeLines = _serviceProvider.GetServices<IPipelineHandler<TRequest>>();
+            var pipeLines = serviceProvider.GetServices<IPipelineHandler<TRequest>>();
             foreach (var pipeline in pipeLines)
             {
                 await pipeline.Handle(request, cancellation);
             }
 
-            var handler = _serviceProvider.GetRequiredService<IRequestHandler<TRequest>>();
+            var handler = serviceProvider.GetRequiredService<IRequestHandler<TRequest>>();
             await handler.Handle(request, cancellation);
         }
 
@@ -41,25 +22,14 @@ namespace Gufel.Dispatcher.Implement
             where TRequest : IRequest<TResponse>
             where TResponse : IResponse
         {
-            var pipeLines = _serviceProvider.GetServices<IPipelineHandler<TRequest, TResponse>>();
+            var pipeLines = serviceProvider.GetServices<IPipelineHandler<TRequest, TResponse>>();
             foreach (var pipeline in pipeLines)
             {
                 await pipeline.Handle(request, cancellation);
             }
 
-            var handler = _serviceProvider.GetRequiredService<IRequestHandler<TRequest, TResponse>>();
+            var handler = serviceProvider.GetRequiredService<IRequestHandler<TRequest, TResponse>>();
             return await handler.Handle(request, cancellation);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            _serviceScope?.Dispose();
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
         }
     }
 }
--- CQRS/Implement/MessagePublisher.cs
+++ CQRS/Implement/MessagePublisher.cs
@@ -1,51 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Concurrent;
-using Gufel.Dispatcher.Base.MessagePublisher;
-using Microsoft.AspNetCore.Http;
+﻿using Gufel.Dispatcher.Base.MessagePublisher;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Gufel.Dispatcher.Implement
 {
-    public class MessagePublisher : IMessagePublisher, IDisposable
+    public sealed class MessagePublisher(IServiceProvider serviceProvider,
+        IMessagePublishStrategy strategy) : IMessagePublisher
     {
-        private readonly IServiceScope? _serviceScope;
-        private readonly IServiceProvider _serviceProvider;
-        private readonly IMessagePublishStrategy _strategy;
-
-        public MessagePublisher(
-            IServiceScopeFactory serviceScope,
-            IHttpContextAccessor httpContextAccessor,
-            IMessagePublishStrategy strategy)
-        {
-            _strategy = strategy;
-            if (httpContextAccessor.HttpContext != null)
-            {
-                _serviceProvider = httpContextAccessor.HttpContext.RequestServices;
-            }
-            else
-            {
-                _serviceScope = serviceScope.CreateScope();
-                _serviceProvider = _serviceScope.ServiceProvider;
-            }
-        }
-
         public void Publish<T>(string topic, T value)
         {
-            var subscribers = _serviceProvider.GetServices<ISubscribeHandler<T>>()
+            var subscribers = serviceProvider.GetServices<ISubscribeHandler<T>>()
                 .Where(x => x.Topic == topic);
-            _strategy.SendMessage(subscribers, value);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            _serviceScope?.Dispose();
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            strategy.SendMessage(subscribers, value);
         }
     }
 }
